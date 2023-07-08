e8ae2d975f6e7a36ca1d83e0706be178cb3a5738
kishor jawale <138974502+kishorsama@users.noreply.github.com>
2023-07-08 15:13:56 +0530

    Add files via upload

---
+++ day 5.py
@@ -0,0 +1,170 @@
+zz=input("enter alphabet : ")
+
+if zz=="a":
+    print("Apple")
+
+elif zz=="A":
+    print("Apple")
+
+elif zz=="b":
+    print("Ball")
+
+elif zz=="B":
+    print("Ball")
+
+elif zz=="c":
+    print("Cat")
+
+elif zz=="C":
+    print("Cat")
+
+elif zz=="d":
+    print("Dog")
+
+elif zz=="D":
+    print("Dog")
+
+elif zz=="e":
+    print("Elephant")
+
+elif zz=="E":
+    print("Elephant")
+
+elif zz=="F":
+    print("Frog")
+
+elif zz=="f":
+    print("Frog")
+
+elif zz=="g":
+    print("Goat")
+
+elif zz=="G":
+    print("Goat")
+
+elif zz=="h":
+    print("Hat")
+
+elif zz=="H":
+    print("Hat")
+
+elif zz=="i":
+    print("Icecream")
+
+elif zz=="I":
+    print("Icecream")
+
+elif zz=="j":
+    print("Joker")
+
+elif zz=="J":
+    print("Joker")
+
+elif zz=="k":
+    print("Kite")
+
+elif zz=="K":
+    print("Kite")
+
+elif zz=="l":
+    print("Leaf")
+
+elif zz=="L":
+    print("Leaf")
+
+elif zz=="M":
+    print("Mango")
+
+elif zz=="m":
+    print("Mango")
+
+elif zz=="N":
+    print("Nest")
+
+elif zz=="n":
+    print("Nest")
+
+elif zz=="o":
+    print("Orange")
+
+elif zz=="O":
+    print("Orange")
+
+elif zz=="p":
+    print("Parrot")
+
+elif zz=="P":
+    print("Parrot")
+
+elif zz=="q":
+    print("Queen")
+
+elif zz=="Q":
+    print("Queen")
+
+elif zz=="r":
+    print("Rocket")
+
+elif zz=="R":
+    print("Rocket")
+
+elif zz=="s":
+    print("Science")
+
+elif zz=="S":
+    print("Science")
+
+elif zz=="T":
+    print("Time")
+
+elif zz=="t":
+    print("Time")
+
+elif zz=="U":
+    print("Universe")
+
+elif zz=="u":
+    print("Universe")
+
+elif zz=="v":
+    print("Vector")
+
+elif zz=="V":
+    print("vector")
+
+elif zz=="w":
+    print("Wise")
+
+elif zz=="W":
+    print("Wise")
+
+elif zz=="X":
+    print("Xerox")
+
+elif zz=="x":
+    print("Xerox")
+
+elif zz=="Y":
+    print("Yen")
+
+elif zz=="y":
+    print("Yen")
+
+elif zz=="z":
+    print("Zebra")
+
+elif zz=="Z":
+    print("Zebra")
+
+else: print("dumbass. I said, ALPHABET")
+
+
+
+
+
+
+
+
+
+
+
